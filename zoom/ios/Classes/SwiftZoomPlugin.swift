6144825d54cd93fe11ea2e298a73e5af434a7eb5
Uzir <uzirthapa@gmail.com>
2022-09-04 22:45:58 -0500

    added some logs

--- zoom/ios/Classes/SwiftZoomPlugin.swift
+++ zoom/ios/Classes/SwiftZoomPlugin.swift
@@ -82,7 +82,10 @@ public class SwiftZoomPlugin: NSObject, FlutterPlugin,FlutterStreamHandler , Mob
     
     public func meetingStatus(call: FlutterMethodCall, result: FlutterResult) {
         
+        print(MobileRTC)
         let meetingService = MobileRTC.shared().getMeetingService()
+        
+        print("meeting Service: \(meetingService)")
         if meetingService != nil {
             
             let meetingState = meetingService?.getMeetingState()
